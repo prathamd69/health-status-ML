1f537253c2fd90b861287e545230cc0a6f340a82
prathamd69 <prathamd2710@gmail.com>
2026-06-15 14:36:12 +0530

    Load data functionality added.

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,7 +3,8 @@ import os
 from sklearn.model_selection import train_test_split
 import logging
 import yaml
-
+import requests
+from io import StringIO
 
 # Logging Setup and Configuration
 
@@ -26,3 +27,41 @@ fileHandler.setFormatter(formatter)
 
 logger.addHandler(consoleHandler)
 logger.addHandler(fileHandler)
+
+
+def loadData(dataURL : str) -> pd.DataFrame:
+    """
+    loadData takes a dataset URL as an input and returns the panda DataFrame of the dataset.
+    Exceptions such as invalid URL, connection timeout, failed parsing are handles and logged.
+    """
+
+    try:
+        response = requests.get(dataURL, timeout=10)
+        response.raise_for_status()
+
+        df = pd.read_csv(StringIO(response.text))
+        logger.debug('Data loaded from %s', dataURL)
+        return df
+    
+    except requests.exceptions.HTTPError:
+        logger.error("The web page could not be found or is private.")
+        raise
+
+    except requests.exceptions.ConnectionError:
+        logger.error("Could not connect. Check your internet connection.")
+        raise
+
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error while loading the data: %s', e)
+        raise
+
+def main():
+    dataURL = input("Enter the URL - ")
+    df = loadData(dataURL=dataURL)
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
